0622d3cbcf328d7052b8b6b99f7795828f3f5a03
Juliana <jlopker@gmail.com>
2015-10-13 14:41:55 -0700

    added ejs

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,5 @@
 // On page load
 $(document).ready(function(){
-  pageLoad();
+  
 });
 
-// function definitions
-
-function pageLoad() {
-  // load foods
-  // set event listeners
-  $("#new-food-form").on("submit", function(e){
-    // prevent form submission
-    e.preventDefault();
-    // post serialized form to server
-    $.post("/api/foods", $(this).serialize(), function(response){
-      // append new food to the page
-      var newFood = response;
-      $("#food-ul").prepend("<li class='list-group-item'>" + newFood.name + 
-        " <span class='label label-default'>"+newFood.yumminess+"</span>" +
-        "<button data-id="+newFood.id+" onclick='deleteFood(this)' type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
-        "</li>");
-      // clear new food form
-      $("#new-food-form")[0].reset();
-    });
-  });
-}
--- server.js
+++ server.js
@@ -35,16 +35,6 @@ app.get("/api/foods", function (req, res){
   res.json(foods);
 });
 
-app.post("/api/foods", function (req, res){
-  var newFood = req.body;
-  // add a unique id
-  newFood.id = foods[foods.length - 1].id + 1;
-  // add new food to DB (which, in this case, is an array)
-  foods.push(newFood);
-  // send a response with newly created object
-  res.json(newFood);
-});
-
 app.listen(3000, function (){
   console.log("listening on port 3000");
 });
